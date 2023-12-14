50e3d275fdf0d2141bf479940589c06b5fd9edd0
godinlu <luc.villaroux@orange.fr>
2023-12-14 08:26:04 +0100

    envoie de la forêt

--- src/functions.py
+++ src/functions.py
@@ -10,7 +10,12 @@ def get_dataset()->tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
     X_train = train_set["comment_text"]  # Remplacez par les noms de vos colonnes cibles
     y_train = train_set.drop(["id","comment_text"], axis=1)
 
-    X_test = pd.read_csv("data/test.csv")["comment_text"]
-    y_test = pd.read_csv("data/test_labels.csv").drop(['id'],axis=1)
+    # Jointure sur la colonne "ID"
+    test_set = pd.merge(pd.read_csv("data/test.csv"), pd.read_csv("data/test_labels.csv"), on='id')
+    test_set.loc(test_set['toxic'] != -1, inplace=True)
+
+    X_test = train_set["comment_text"]
+    y_test = train_set.drop(["id","comment_text"], axis=1)
+
     return (X_train, y_train, X_test, y_test)
 
