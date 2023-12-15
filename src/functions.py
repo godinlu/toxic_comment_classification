07a18c39c88b6456df2a71a4119ca242ca7e029b
Groot38 <soheil.mecheloukh@etu.univ-grenoble-alpes.fr>
2023-12-15 09:36:02 +0100

    fonction pit pour tester en supprimant les données

--- src/functions.py
+++ src/functions.py
@@ -19,3 +19,39 @@ def get_dataset()->tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
 
     return (X_train, y_train, X_test, y_test)
 
+def get_pit()->tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """this function return all of the dataSet of the chat like this
+        (X_train, y_train, X_test, y_test)
+    """
+    # on commence par récupérer le jeu de données
+    train_set = pd.read_csv("data/train.csv")
+
+
+
+
+    # Sélectionner les lignes qui ont des valeurs non nulles dans au moins une colonne
+    non_zero_rows = train_set[(train_set.iloc[:, 2:] != 0).any(axis=1)]
+
+    # Sélectionner les lignes qui ont que des zéros et en prendre environ 50%
+    only_zeros = train_set[(train_set.iloc[:, 2:] == 0).all(axis=1)].sample(frac=0.5)
+
+    # Concaténer les deux ensembles de données
+    result = pd.concat([non_zero_rows, only_zeros])
+    print(result.shape)
+
+
+    X_train = train_set["comment_text"]  # Remplacez par les noms de vos colonnes cibles
+    y_train = train_set.drop(["id","comment_text"], axis=1)
+
+
+
+
+
+    # Jointure sur la colonne "ID"
+    test_set = pd.merge(pd.read_csv("data/test.csv"), pd.read_csv("data/test_labels.csv"), on='id')
+    test_set = test_set[test_set['toxic'] != -1]
+
+    X_test = test_set["comment_text"]
+    y_test = test_set.drop(["id","comment_text"], axis=1)
+
+    return (X_train, y_train, X_test, y_test)
